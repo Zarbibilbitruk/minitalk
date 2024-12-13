b147b1f9ba6a96a9b0f00888c39c6b38997aa62b
Titouan Autin--Desbois <tautin--@paul-f3Ar4s6.clusters.42paris.fr>
2024-12-13 19:27:25 +0100

    oueee jai rien fait

--- server.c
+++ server.c
@@ -1,31 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tautin-- <tautin--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/11 19:11:14 by tautin--          #+#    #+#             */
-/*   Updated: 2024/12/13 17:49:23 by tautin--         ###   ########.fr       */
+/*   Created: 2024/12/13 17:46:45 by tautin--          #+#    #+#             */
+/*   Updated: 2024/12/13 18:04:08 by tautin--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	main(int ac, char **av)
+int	main(void)
 {
-	int		id;
-	char	*msg;
+	printf("PID : %d\n", getpid());
 
-	if (ac == 3)
-	{
-		id = ft_atoi(av[1]);
-		if (!id)
-			return (ft_putstr_fd(message derreur));
-		msg = av[2];
-		if (!msg)
-			return (ft_putstr_fd(message derreur));
-		send_msg(msg); // a faire
-	}
-	return (ft_putstr_fd(message derreur)); // a faire
+
+	while (1)
+		pause();
+	return (EXIT_SUCCESS);
 }
