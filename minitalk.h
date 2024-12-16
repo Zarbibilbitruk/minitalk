f6ae28a9372a1c1ad3933e9e29fd232f69be8de0
Titouan Autin--Desbois <tautin--@paul-f3Br7s6.clusters.42paris.fr>
2024-12-16 19:28:21 +0100

    random

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: tautin-- <tautin--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 19:11:41 by tautin--          #+#    #+#             */
-/*   Updated: 2024/12/13 19:27:04 by tautin--         ###   ########.fr       */
+/*   Updated: 2024/12/16 19:27:55 by tautin--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define MINITALK_H
 
 # include <signal.h>
+# include <asm-generic/siginfo.h>
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
--- server.c
+++ server.c
@@ -6,16 +6,22 @@
 /*   By: tautin-- <tautin--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 17:46:45 by tautin--          #+#    #+#             */
-/*   Updated: 2024/12/13 18:04:08 by tautin--         ###   ########.fr       */
+/*   Updated: 2024/12/16 19:27:58 by tautin--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	handler(int signa, siginfo_t *info, )
+{
+	
+}
+
 int	main(void)
 {
 	printf("PID : %d\n", getpid());
 
+	Signal(SIGUSR1, handler); //a faire
 
 	while (1)
 		pause();
